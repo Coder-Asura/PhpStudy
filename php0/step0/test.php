b5071f25a7f32e48107a208248fc90ba1d0e55d8
Asura <709238339@qq.com>
2018-04-17 18:14:50 +0800

    Initial commit

---
+++ php0/step0/test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Asura
+ * Date: 2018/4/17
+ * Time: 16:47
+ */
+echo "Hello World";
+echo 2 + 3;
+echo 2 * 3;
+//错误写法
+//echo "Hello"+" World";
+echo "Hello" . " World";
